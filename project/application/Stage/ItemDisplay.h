41b42635e0e498b6795e753041a58187c6389e34
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2026-09-07 15:37:49 +0900

    テキストで表示アイテムの情報を表示可能にした

--- project/application/Stage/ItemDisplay.h
+++ project/application/Stage/ItemDisplay.h
@@ -10,34 +10,47 @@
 //  プレイヤーがアイテムを離すと元の位置に戻る
 //
 // =============================================================================
-class ItemDisplay {
-public:
-	// 初期化処理
-	void Initialize();
-	// 更新処理
-	// アイテムを持っているかどうかでUIの挙動が変わる
-	void Update(bool hasItem);
-	// 描画
-	void Draw();
-private:
-	// EaseInOutBack
-	float EaseInOutBack(float t);
-private:
-	// スプライト本体
-	std::unique_ptr<TuboEngine::Sprite> sprite_;
-	// 前回のアイテム所持状態
-	bool previousHasItem_ = false;
-	// Sprite移動中か
-	bool ismoving_ = false;
-	// true  : START → END
-	// false : END → START
-	bool moveforward_ = false;
-	// イージング経過時間
-	float easetime_;
-	// 移動時間
-	float easeduration_;
-	// 開始位置
-	TuboEngine::Math::Vector2 startpos_;
-	// 終了位置
-	TuboEngine::Math::Vector2 endpos_;
-};
+namespace game {
+
+	class Item;
+
+	class ItemDisplay {
+	public:
+		// 初期化処理
+		void Initialize();
+		// 更新処理
+		// 現在持っているアイテムを渡す 
+		// nullptr = 何も持っていない 
+		void Update(Item* item);
+		// 描画
+		void Draw();
+	private:
+		// EaseInOutBack
+		float EaseInOutBack(float t);
+		// アイテムの種類に応じてテキストを変更 
+		void UpdateItemText(Item* item);
+		// アイテムのパラメータを取得
+		void UpdateItemParameter(Item* item);
+		std::string FormatFloat(float value);
+	private:
+		// スプライト本体
+		std::unique_ptr<TuboEngine::Sprite> sprite_;
+		// 現在表示しているアイテム 
+		Item* currentItem_ = nullptr;
+		// 前回のアイテム所持状態
+		bool previousHasItem_ = false;
+		// Sprite移動中か
+		bool ismoving_ = false;
+		// true  : START → END
+		// false : END → START
+		bool moveforward_ = false;
+		// イージング経過時間
+		float easetime_;
+		// 移動時間
+		float easeduration_;
+		// 開始位置
+		TuboEngine::Math::Vector2 startpos_;
+		// 終了位置
+		TuboEngine::Math::Vector2 endpos_;
+	};
+}
